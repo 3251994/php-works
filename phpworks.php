7fa075d8a207918fc747d9b866cdeb9e4d239074
Anitson T T <anitsonttsat@gmail.com>
2021-06-25 13:33:31 +0530

    Update phpworks.php

--- phpworks.php
+++ phpworks.php
@@ -5,8 +5,39 @@
 <h1>My first PHP page</h1>
 
 <?php
-echo "Hello World!";
-?>
+//echo "Hello World!";
+//echo strlen("Hello world!");
+//echo str_word_count("Hello world!"); 
+//$x = 1.9e411;
+//var_dump($x);
+    
+    //$x = acos(8);
+//var_dump($x);
+    
+    //$x = 5985;
+//var_dump(is_numeric($x));
 
+//$x = "5985";
+//var_dump(is_numeric($x));
+
+//$x = "59.85" + 100;
+//var_dump(is_numeric($x));
+
+//$x = "Hello";
+//var_dump(is_numeric($x));
+    
+    //$x = 23465.768;
+//$int_cast = (int)$x;
+//echo $int_cast;
+
+//echo "<br>";
+    
+    //$x = "23465.768";
+//$int_cast = (int)$x;
+//echo $int_cast;
+    
+    echo(rand());
+    echo(rand(10, 100));
+?>
 </body>
 </html>
